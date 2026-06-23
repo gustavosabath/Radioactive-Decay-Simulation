b54c485112e4a7f74f5837c15949e99b4a6e688c
Gustavo Sabath <gustavosabath@gmail.com>
2026-06-23 16:47:10 -0300

    Alterations

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ def euler_decay(l, n0, h, size):
 
 
 n0 = 50000
-T_half = 5730
+T_half = 5730 # anos]
+uranium = 4e9
 l = np.log(2)/T_half
-h = 2000
+h = 10
 t = np.arange(0, 100000, h)
 N_analytic = n0*np.exp(-l*t)
 N_numeric = euler_decay(l, n0, h, len(t))
 
 plt.plot(t, N_analytic, label="Analytic solution", color='#000000')
 plt.plot(t, N_numeric, label="Numeric solution", linestyle='--')
+
 plt.xlim(0,50000)
 plt.ylim(0, 50000)
 plt.legend()
